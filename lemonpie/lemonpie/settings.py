9ec38490383b06b21e2ab961dd9aac61b5e53768
lshao <laetitia.shao@gmail.com>
2017-08-31 18:59:40 +0200

    Add floppyforms

--- lemonpie/lemonpie/settings.py
+++ lemonpie/lemonpie/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_countries',
+    'floppyforms',
     'phonenumber_field',
     'polymorphic',
     'resumebuilder.apps.ResumeBuilderConfig',
