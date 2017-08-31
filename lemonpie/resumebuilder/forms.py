0d7a2733d55cf1ffbef94ffc9c9079c1494135cf
lshao <laetitia.shao@gmail.com>
2017-08-31 18:55:41 +0200

    more experimentation with widgets

--- lemonpie/resumebuilder/forms.py
+++ lemonpie/resumebuilder/forms.py
@@ -3,7 +3,13 @@ from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 from django_countries import countries
 from django_countries.fields import LazyTypedChoiceField
-from .widgets import EmailWidget, TextWidget, DatePickerWidget
+from .widgets import (
+    EmailWidget,
+    TextWidget,
+    DatePickerWidget,
+    PhoneWidget,
+    CountryPickerWidget,
+    SkillLevelWidget)
 
 class EntryForm(forms.Form):
     name = forms.CharField(
@@ -12,10 +18,21 @@ class EntryForm(forms.Form):
 
 
 class SkillForm(EntryForm):
+    SKILL_LEVEL_CHOICES = (
+      (1, 1),
+      (2, 2),
+      (3, 3),
+      (4, 4),
+      (5, 5)
+    )
     skill_name = forms.CharField(
         max_length=50,
         widget=TextWidget)
-    skill_level = forms.IntegerField(min_value=1, max_value=5)
+    skill_level = forms.IntegerField(
+        min_value=1,
+        max_value=5,
+        widget=SkillLevelWidget(choices=SKILL_LEVEL_CHOICES),
+        )
 
 
 class PersonalForm(EntryForm):
@@ -25,7 +42,9 @@ class PersonalForm(EntryForm):
     given_name = forms.CharField(
         max_length=50,
         widget=TextWidget)
-    phone_number = PhoneNumberField()
+    phone_number = PhoneNumberField(
+        widget=PhoneWidget
+    )
     email_address = forms.EmailField(
         max_length=50,
         widget=EmailWidget)
@@ -35,8 +54,10 @@ class ActivityForm(EntryForm):
     location_city = forms.CharField(
         max_length=50,
         widget=TextWidget)
-    location_country = LazyTypedChoiceField(choices=countries)
-    #TODO: Add a widget for Date selection
+    location_country = LazyTypedChoiceField(
+        choices=countries,
+        widget=CountryPickerWidget,
+    )
     date_begin = forms.DateField(
       widget=DatePickerWidget
     )
--- lemonpie/resumebuilder/widgets.py
+++ lemonpie/resumebuilder/widgets.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.html import mark_safe
+from django_countries.widgets import CountrySelectWidget
 
 class EmailWidget(forms.widgets.TextInput):
     def render(self, name, value, attrs=None):
@@ -7,7 +8,7 @@ class EmailWidget(forms.widgets.TextInput):
         attrs['placeholder'] = "Email address"
         html = super(EmailWidget, self).render(name, value, attrs)
         html = '''
-        <div class="input-group">
+        <div class="input-group col-xs-6">
         <span class="input-group-addon">
           <i class="fa fa-envelope"></i>
         </span>
@@ -21,7 +22,20 @@ class TextWidget(forms.widgets.TextInput):
         attrs['placeholder'] = name
         html = super(TextWidget, self).render(name, value, attrs)
         html = '''
-        <div class="input-group">
+        <div class="input-group col-xs-6">
+        ''' + html + '</div>'
+        return mark_safe(html)
+
+class PhoneWidget(forms.widgets.TextInput):
+    def render(self, name, value, attrs=None):
+        attrs['class'] = "form-control"
+        attrs['placeholder'] = "international format phone number"
+        html = super(PhoneWidget, self).render(name, value, attrs)
+        html = '''
+        <div class="input-group col-xs-6">
+        <span class="input-group-addon">
+            <i class="fa fa-phone"></i>
+        </span>
         ''' + html + '</div>'
         return mark_safe(html)
 
@@ -33,7 +47,7 @@ class DatePickerWidget(forms.widgets.TextInput):
         attrs['id'] = "datepicker"
         html = super(DatePickerWidget, self).render(name, value, attrs)
         html = '''
-        <div class="input-group date">
+        <div class="input-group date col-xs-4">
         <span class="input-group-addon">
           <i class="fa fa-calendar"></i>
         </span>
@@ -41,4 +55,20 @@ class DatePickerWidget(forms.widgets.TextInput):
         html = html + '''
         </div>
         '''
+        return mark_safe(html)
+
+class CountryPickerWidget(CountrySelectWidget):
+
+    def render(self, name, value, attrs=None):
+        attrs['class'] = "form-control"
+        html = super(CountryPickerWidget, self).render(name, value, attrs)
+        return mark_safe(html)
+
+class SkillLevelWidget(forms.RadioSelect):
+    def render(self, name, value, attrs=None):
+        attrs['class'] = "form-control"
+        html = super(SkillLevelWidget, self).render(name, value, attrs)
+        html = '''
+        <div class="input-group">
+        ''' + html + '</div>'
         return mark_safe(html)
